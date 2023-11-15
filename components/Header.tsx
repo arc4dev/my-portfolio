19d40c4dbe0ec415c1ac6a84814e7beb5d189d29
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-11-15 21:01:30 +0100

    feat: navbar mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,18 +3,20 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 import { IoIosSend } from 'react-icons/io';
 import Navbar from './Navbar';
 import Link from 'next/link';
+import NavbarModal from './NavbarModal';
 
 type Props = {};
 
 const Header = (props: Props) => {
-  const [isNavModalOpen, setIsNavModalOpen] = useState();
+  const [isNavModalOpen, setIsNavModalOpen] = useState<true | false>(false);
 
   return (
-    <header className="z-50 fixed top-0 w-full flex justify-center my-4 px-4">
+    <header className="z-50 fixed top-0 w-full flex justify-center px-4 py-3 sm:my-4 bg-transparent">
       <div className="flex justify-between items-center w-full sm:hidden">
-        <button>
+        <button onClick={() => setIsNavModalOpen((v) => !v)}>
           <RxHamburgerMenu className="w-8 h-8" />
         </button>
+
         <Link
           href="#contact"
           className="flex items-center gap-2 text-[0.9rem] group">
@@ -23,6 +25,11 @@ const Header = (props: Props) => {
         </Link>
       </div>
       <Navbar />
+
+      <NavbarModal
+        isModalOpen={isNavModalOpen}
+        onCloseModal={() => setIsNavModalOpen(false)}
+      />
     </header>
   );
 };
--- components/NavbarModal.tsx
+++ components/NavbarModal.tsx
@@ -1,19 +1,58 @@
 import React from 'react';
-import ReactModal from 'react-modal';
+import { motion } from 'framer-motion';
+import { HiOutlineX } from 'react-icons/hi';
+import { createPortal } from 'react-dom';
+import { navLinks } from '../data/navLinks';
+import Link from 'next/link';
+import { useActiveSection } from '../contexts/ActiveSectionContext';
+import SocialLinks from './SocialLinks';
+import { SectionName } from '../lib/types';
 
-type Props = {};
+type Props = {
+  isModalOpen: boolean;
+  onCloseModal: () => void;
+};
 
-// we need to store the state of the modal in a context to retrieve it and use it in the header
-const NavbarModal = ({}: Props) => {
-  return (
-    <ReactModal
-      isOpen={isModalOpen}
-      onRequestClose={handleClick}
-      className=""
-      contentLabel="Navbar Modal"
-      appElement={document.getElementById('root')}>
-      NavbarModal
-    </ReactModal>
+const NavbarModal = ({ isModalOpen, onCloseModal }: Props) => {
+  const { setActiveSection } = useActiveSection();
+
+  const handleClickLink = (linkName: SectionName) => {
+    setActiveSection(linkName);
+    onCloseModal();
+  };
+
+  return createPortal(
+    <>
+      {isModalOpen && (
+        <motion.div
+          className="fixed inset-0 w-full h-full bg-white dark:bg-bgColorDarker z-50 px-4 pt-36 pb-4 flex flex-col justify-between"
+          initial={{ x: -640 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.4, type: 'tween' }}>
+          <button onClick={onCloseModal} className="absolute top-3 right-3">
+            <HiOutlineX className="w-10 h-10 text-gray-900 dark:text-gray-100" />
+          </button>
+
+          <ul className="flex flex-col gap-4 font-semibold tracking-wide py-2.5 px-2 text-2xl">
+            {navLinks.map((link) => (
+              <li key={link.name}>
+                <Link
+                  className="rounded-full px-3.5 py-1.5 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100/80 transition duration-200 relative"
+                  onClick={() => handleClickLink(link.name)}
+                  href={link.hash}>
+                  {link.name.charAt(0).toUpperCase() + link.name.slice(1)}
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+          <div className="pl-3">
+            <SocialLinks />
+          </div>
+        </motion.div>
+      )}
+    </>,
+    document.body
   );
 };
 
