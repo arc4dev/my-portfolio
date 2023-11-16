c4a366457af888f69df3e92aa5901e0b0dfdc79a
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-11-16 22:25:58 +0100

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -10,4 +12,12 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  ...withPWA({
+    pwa: {
+      dest: 'public',
+      disable: process.env.NODE_ENV === 'development',
+    },
+  }),
+};
